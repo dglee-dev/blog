ccbe71537758bf135641a5168f7cfacce1fdaf2f
donggyulee <nninnnin7@gmail.com>
2025-07-02 16:32:47 +0900

    라우터 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import styled from "styled-components/macro";
 import React from "react";
+import styled from "styled-components/macro";
 
 const Header = ({ children }) => {
   return <Container>{children}</Container>;
---
+++ src/pages/Works.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Resume from "@/components/Resume";
+import Footer from "@/components/Footer";
+
+const WorksPage = () => {
+  return (
+    <div>
+      <Resume />
+      <Footer />
+    </div>
+  );
+};
+
+export default WorksPage;
