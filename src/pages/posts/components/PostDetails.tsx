a2feb45895cbf121d54b565a2caf5cd2eb0afa43
donggyulee <nninnnin7@gmail.com>
2025-07-29 17:46:12 +0900

    Complete fetching posts

--- src/pages/posts/components/PostDetails.tsx
+++ src/pages/posts/components/PostDetails.tsx
@@ -1,13 +1,26 @@
-import React, { useEffect } from "react";
+import React from "react";
+import styled from "styled-components";
 
-const PostDetails = ({
-  src,
-}: {
-  src: string;
-}) => {
-  useEffect(() => {}, []);
+import usePost from "@/pages/posts/hooks/usePost";
 
-  return <div></div>;
+const PostDetails = () => {
+  const { contents } = usePost();
+
+  return (
+    <Container>
+      <Paragraph>{contents}</Paragraph>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const Paragraph = styled.p`
+  padding: 1em;
+  margin: 0;
+`;
 
 export default PostDetails;
