f64caa343155f7c5485bbbfa145ca285ffc331f2
이동규 (Justin) <nninnnin7@gmail.com>
2025-09-07 18:17:51 +0900

    `lib/router`: Update Route component test

--- src/shared/lib/router/tests/Route.test.tsx
+++ src/shared/lib/router/tests/Route.test.tsx
@@ -7,22 +7,11 @@ import "@testing-library/jest-dom";
 
 import Route from "@/shared/lib/router/components/Route";
 
-describe.only("lorem what", () => {
-  test("first", () => {
-    const { container, asFragment } = render(
-      <Route
-        path="/abc"
-        element={<div>abc</div>}
-      />
-    );
+test.only("Route component returns null", () => {
+  const { container } = render(
+    <Route path="/abc" element={<div>abc</div>} />
+  );
 
-    expect(container.firstChild).toBeNull();
-    expect(container).toBeEmptyDOMElement();
-
-    const fragment = asFragment();
-
-    expect(fragment).toMatchInlineSnapshot(
-      `<DocumentFragment />`
-    );
-  });
+  expect(container.firstChild).toBeNull();
+  expect(container).toBeEmptyDOMElement(); // assert no visible content for the user
 });
